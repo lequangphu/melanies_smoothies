8b946d60599025214f0e70503070b56e85baf7c6
Phu <33654804+lequangphu@users.noreply.github.com>
2026-04-07 08:23:49 +0700

    Refactor: Move requests import to top and API call inside fruit loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app.
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -26,8 +27,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-
-    # st.write(ingredients_string)
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/watermelon"
+        )
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(), use_container_width=True
+        )
 
     my_insert_stmt = (
         """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -38,19 +43,8 @@ if ingredients_list:
         + """')"""
     )
 
-    # st.write(my_insert_stmt)
-    # st.stop()
-
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered!", icon="✅")
-
-import requests
-
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
